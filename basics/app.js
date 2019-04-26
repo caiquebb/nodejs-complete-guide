830c3bde824a6cd7bb6d570e149dfb9b3e0ca725
Caique Benassi Bertolozzi <caique.bertolozzi@agv.com.br>
2019-04-26 15:59:42 -0300

    Finish basics

--- basics/app.js
+++ basics/app.js
@@ -1,14 +1,8 @@
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
