3f6fb5a381dd493328c8896f13e0e99cfb3eaf4e
Caique Benassi Bertolozzi <caique.bertolozzi@agv.com.br>
2019-06-17 11:47:37 -0300

    First steps with express

--- express/app.js
+++ express/app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('In the moddleware!');
-    next(); // Allows the request to continue to the next middleware in line
-});
-
-app.use((req, res, next) => {
     console.log('In another moddleware!');
     res.send('<h1>Hello from Express!');
 });
